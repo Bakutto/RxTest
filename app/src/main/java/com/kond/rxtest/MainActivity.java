22bb8e17c5969fa1e7f785acbe79d0bfe40dc19e
Bakutto <oribakuto@gmail.com>
2021-08-19 22:37:09 +0300

    add first Rx

--- app/src/main/java/com/kond/rxtest/MainActivity.java
+++ app/src/main/java/com/kond/rxtest/MainActivity.java
@@ -3,6 +3,12 @@ package com.kond.rxtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Observable<Integer> observable = Observable.just(1, 2, 4);
+        observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.i("MainActivity", String.valueOf(integer));
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 }
