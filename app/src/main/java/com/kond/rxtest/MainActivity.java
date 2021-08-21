84882bbdaaf5d19b1745437eb5c0a0cfb9f9acdc
Bakutto <oribakuto@gmail.com>
2021-08-21 13:32:46 +0300

    init commit

--- app/src/main/java/com/kond/rxtest/MainActivity.java
+++ app/src/main/java/com/kond/rxtest/MainActivity.java
@@ -3,29 +3,29 @@ package com.kond.rxtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String TAG = "MainActivity";
         setContentView(R.layout.activity_main);
-        Observable<Integer> observable = Observable.just(1);
+        Observable<Integer> observable = Observable.just(1, 2, 3, 4);
         observable.subscribe(new Observer<Integer>() {
             @Override
             public void onSubscribe(@NonNull Disposable d) {
-
+                Timber.tag(TAG).e(String.valueOf(d));
             }
 
             @Override
             public void onNext(@NonNull Integer integer) {
-                Log.i("MainActivity", String.valueOf(integer));
+                Timber.tag(TAG).e(String.valueOf(integer));
             }
 
             @Override
